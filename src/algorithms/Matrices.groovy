4c116a7c12412ae3cd66daab81311964797a1797
Phillip Cohen <pc@phillipcohen.net>
2012-02-06 23:52:53 -0600

    Added functions for adding and subtracting matrices.

--- src/algorithms/Matrices.groovy
+++ src/algorithms/Matrices.groovy
@@ -25,6 +25,42 @@ def splitMatrix(matrix) {
 }
 
 /**
+ * Adds the two matrices.
+ */
+def addMatrices(one, two) {
+
+    def returnMatrix = new int[one.size()][two.size()]
+
+    for (int i = 0; i < one.size(); i++)
+        for (int j = 0; j < one[0].size(); j++)
+            returnMatrix[i][j] = one[i][j] + two[i][j]
+
+    return returnMatrix;
+}
+
+/**
+ * Subtracts the two matrices.
+ */
+def subtractMatrices(one, two) {
+    return addMatrices(one, scalarMultiply(two, -1))
+}
+
+/**
+ * Multiplies every value in the matrix by the given scalar.
+ * Not a normal matrix function; this is mostly used as a helper method.
+ */
+def scalarMultiply(matrix, scalar) {
+
+    def returnMatrix = new int[matrix.size()][matrix[0].size()];
+
+    for (int i = 0; i < matrix.size(); i++)
+        for (int j = 0; j < matrix[0].size(); j++)
+            returnMatrix[i][j] = matrix[i][j] * scalar;
+
+    return returnMatrix;
+}
+
+/**
  * Selects a square subset of the given matrix, returned as a new, smaller matrix.
  * The new matrix has size [numRows][numColumns] and is selected from the offset [row][column].
  */
@@ -43,5 +79,27 @@ def selectSubset(matrix, int row, int column, int numRows, int numColumns) {
 // TESTING
 //==============
 
-assert splitMatrix([[1, 3], [7, 5]]) == [topLeft: [[1]], topRight: [[3]], bottomLeft: [[7]], bottomRight: [[5]]]
-assert splitMatrix([[1, 3, 9, 3], [7, 5, 9, 1], [9, 3, -8, 4], [8, -2, -4, 6]]) == [topLeft: [[1, 3], [7, 5]], topRight: [[9, 3], [9, 1]], bottomLeft: [[9, 3], [8, -2]], bottomRight: [[-8, 4], [-4, 6]]]
+def test() {
+
+    def matrixOne = [[1, 3], [7, 5]]
+
+    // Test splitting...
+    assert splitMatrix(matrixOne) == [topLeft: [[1]], topRight: [[3]], bottomLeft: [[7]], bottomRight: [[5]]]
+    assert splitMatrix([[1, 3, 9, 3], [7, 5, 9, 1], [9, 3, -8, 4], [8, -2, -4, 6]]) == [topLeft: [[1, 3], [7, 5]], topRight: [[9, 3], [9, 1]], bottomLeft: [[9, 3], [8, -2]], bottomRight: [[-8, 4], [-4, 6]]]
+
+    // Test adding...
+    assert addMatrices(matrixOne, [[7, 4], [-7, 0]]) == [[8, 7], [0, 5]]
+    assert addMatrices(matrixOne, new int[matrixOne.size()][matrixOne[0].size()]) == matrixOne
+
+    // Test scalar multiply...
+    assert scalarMultiply(matrixOne, -1) == [[-1, -3], [-7, -5]]
+    assert scalarMultiply(matrixOne, 0) == new int[matrixOne.size()][matrixOne[0].size()]
+    
+    // Test subtracting...
+    assert subtractMatrices(matrixOne, [[7, 4], [-7, 0]]) == [[-6, -1], [14, 5]]
+    assert subtractMatrices(matrixOne, new int[matrixOne.size()][matrixOne[0].size()]) == matrixOne
+
+    println "Success!"
+}
+
+test();
