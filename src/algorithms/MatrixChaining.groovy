b9b2ef9e0d66cf280caf907577277e7dd266052e
Phillip Cohen <pc@phillipcohen.net>
2012-02-20 23:06:52 -0600

    Fixed bug. Groovy ranges, like the ranges in the book, use <= on the upper bound, so they are inclusive.

--- src/algorithms/MatrixChaining.groovy
+++ src/algorithms/MatrixChaining.groovy
@@ -26,15 +26,15 @@ class MatrixChaining {
         def cutPoints = new int[dimensions.size()][dimensions.size()]
 
         // Test all possible chain sizes from 2 to the size of all the dimensions.
-        for (int chainSize = 2; chainSize < dimensions.length - 1; chainSize++) {
+        for (int chainSize = 2; chainSize <= dimensions.length - 1; chainSize++) {
 
-            for (int left = 1; left < dimensions.length - chainSize; left++) {
+            for (int left = 1; left <= dimensions.length - chainSize; left++) {
 
                 int right = left + chainSize - 1;
                 cost[left][right] = Integer.MAX_VALUE;
 
                 // Test all possible split points.
-                for (int splitPoint = left; splitPoint < right - 1; splitPoint++) {
+                for (int splitPoint = left; splitPoint < right; splitPoint++) {
 
                     // What's the potential cost for this split?
                     // This is equal to the costs of the left and right side, PLUS the cost to combine the two. (Which will be a multiplication itself.)
@@ -66,10 +66,14 @@ class MatrixChaining {
     /**
      * Nicely prints a 2D array.
      */
-    static void print2dArray(array) {
+    static void print2dArray(array, int spacing) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++)
-                System.out.print(String.format("%8s", array[i][j]));
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j])
+                    System.out.print(String.format("%${spacing}s", array[i][j]));
+                else
+                    System.out.print(String.format("%${spacing}s", " "));
+            }
             System.out.println();
         }
     }
@@ -80,13 +84,13 @@ class MatrixChaining {
         def results = calculateChain(dimensions)
 
         print "\nResults: "
-        //  printParentheses(results.cutPoints, 0, dimensions.length - 1);
+//        printParentheses(results.cutPoints, 0, dimensions.length - 1);
 
         println "\nCosts table (m):"
-        print2dArray(results.cost)
+        print2dArray(results.cost, 8)
 
         println "\nCut points table (s):"
-        print2dArray(results.cutPoints)
+        print2dArray(results.cutPoints, 8)
     }
 
     /**
@@ -95,10 +99,10 @@ class MatrixChaining {
     static test() {
 
         int[] testDimensions = [30, 35, 15, 5, 10, 20, 25]
-//        printResults(testDimensions)
+        printResults(testDimensions)
 
         int[] testDimensions2 = [5, 10, 3, 12, 5, 50, 6]
-        printResults(testDimensions)
+        printResults(testDimensions2)
 //
     }
 
