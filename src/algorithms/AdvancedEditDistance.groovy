5ce9486c1210525b9a10f113a31b61cd3952a396
Phillip Cohen <pc@phillipcohen.net>
2012-03-05 23:04:58 -0600

    Started to add AdvancedEditDistance, which is like EditDistance but it supports user-adjusted costs, and more operations.

--- src/algorithms/AdvancedEditDistance.groovy
+++ src/algorithms/AdvancedEditDistance.groovy
@@ -3,41 +3,55 @@ package algorithms
 /**
  * Algorithms to determine the differences between two strings.
  *
- * @see http://en.wikipedia.org/wiki/Levenshtein_distance
+ * This is like EditDistance but with more operations -- twiddle and kill.
+ *  Twiddle = insert the next two characters in reverse order
+ *  Kill = stop the string here
+ *  Copy = same as "skip" in basic mode but (potentially) with a cost
+ *
+ * Also, the costs for each operation must be user-customizable.
+ *
+ * @see "Algorithms", Problem 15-5, "Edit Distance"
  */
-class EditDistance {
+class AdvancedEditDistance {
 
     static enum BasicAction {
-        Insert, Delete, Replace, Skip
+        Insert, Delete, Replace, Copy, Twiddle, Kill
+    }
+
+
+    static getDefaultCosts() {
+        ["Copy": 0, "Replace": 2, "Delete": 5, "Insert": 5, "Twiddle": 7, "Kill": 7]
     }
 
     /**
      * Returns the minimum number of operations needed to change the string one into two.
-     * Supports these operations: Insert, Delete, Replace, Skip.
      */
-    static def computeLevenshteinDistance(String one, String two) {
+    static def computeLevenshteinDistance(String one, String two, costs) {
 
         def computations = new int[two.length() + 1][one.length() + 1]
 
         // Prefix the special case for the first row and column.
         for (int j = 1; j <= one.length(); j++)
-            computations[0][j] = j;
+            computations[0][j] = j * costs["Delete"];
         for (int i = 1; i <= two.length(); i++)
-            computations[i][0] = i;
+            computations[i][0] = i * costs["Insert"];
 
         for (int j = 1; j <= one.length(); j++) {
             for (int i = 1; i <= two.length(); i++) {
 
-                // Which of these three is cheapest?
-                int deletionCost = computations[i - 1][j] + 1;
-                int additionCost = computations[i][j - 1] + 1;
-                int replacementCost = computations[i - 1][j - 1] + ((two.charAt(i - 1) == one.charAt(j - 1)) ? 0 : 1);
+                boolean copyAllowed = (two.charAt(i - 1) == one.charAt(j - 1));
 
-                computations[i][j] = threeWayMin(additionCost, deletionCost, replacementCost)
+                // Which of these three is cheapest?
+                int deletionCost = costs["Delete"] + computations[i - 1][j];
+                int insertCost = costs["Insert"] + computations[i][j - 1];
+                int copyCost = copyAllowed ? (costs["Copy"] + computations[i - 1][j - 1]) : Integer.MAX_VALUE;
+                int replaceCost = costs["Replace"] + computations[i - 1][j - 1];
+
+                int replacementCost = computations[i][j] = fourWayMin(insertCost, deletionCost, copyCost, replaceCost)
             }
         }
 
-        [one: one, two: two, cost: computations[two.length()][one.length()], table: computations]
+        [one: one, two: two, cost: computations[two.length()][one.length()], costs: costs, table: computations]
     }
 
     /**
@@ -55,7 +69,9 @@ class EditDistance {
 
             // Determine which edit was made by seeing which neighbor (insert, delete, modify/skip) is cheapest.
             // Then push this action to the top of the "actions" stack.
+                        
             def cheapestNeighbor = threeWayMin(result.table[i - 1][j], result.table[i][j - 1], result.table[i - 1][j - 1])
+
             if (cheapestNeighbor == result.table[i - 1][j]) { // Insert
                 actions.add(0, BasicAction.Insert);
                 i--;
@@ -65,8 +81,8 @@ class EditDistance {
             } else { // Replace
 
                 // Was it a replacement, or just a skip?
-                if (result.table[i - 1][j - 1] == result.table[i][j])
-                    actions.add(0, BasicAction.Skip);
+                if (result.table[i][j] == result.costs["Copy"] + result.table[i - 1][j - 1])
+                    actions.add(0, BasicAction.Copy);
                 else
                     actions.add(0, BasicAction.Replace);
 
@@ -97,13 +113,19 @@ class EditDistance {
     }
 
     /**
+     * Returns the mininum of the given four numbers.
+     */
+    static int fourWayMin(first, second, third, fourth) {
+        return (int) Math.min(Math.min(Math.min(first, second), third), fourth);
+    }
+
+    /**
      * Tests all the algorithms.
      */
     static test() {
-        prettyPrint(computeLevenshteinDistance("kitten", "sitting"));
-        prettyPrint(computeLevenshteinDistance("Saturday", "Sunday"));
+        prettyPrint(computeLevenshteinDistance("kitten", "sitting", defaultCosts));
+        prettyPrint(computeLevenshteinDistance("Saturday", "Sunday", defaultCosts));
     }
 
     static void main(String[] args) { test(); }
-
 }
