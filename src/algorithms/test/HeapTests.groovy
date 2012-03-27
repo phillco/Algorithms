77b1165f56c569ea3aa5170ed13f550e2bf031d6
Phillip Cohen <pc@phillipcohen.net>
2012-03-27 18:41:30 -0500

    Added tests for heaps.

---
+++ src/algorithms/test/HeapTests.groovy
@@ -0,0 +1,30 @@
+package algorithms.test
+
+import algorithms.Heap
+
+/**
+ * Tests heaps!
+ */
+class HeapTests extends GroovyTestCase {
+
+    void testTurnIntoHeap() {
+
+        def list = [5, 4, 8, 9, 2, 0, 9, 7, 2]
+
+        Heap.turnIntoHeap(list);
+        testMaxHeap(list);
+    }
+
+    def testMaxHeap(A, int i = 1) {
+
+        if (Heap.hasLeft(A, i)) {
+            assert A[i] >= A[Heap.left(i)]
+            testMaxHeap(A, Heap.left(i))
+        }
+
+        if (Heap.hasRight(A, i)) {
+            assert A[i] >= A[Heap.right(i)]
+            testMaxHeap(A, Heap.right(i))
+        }
+    }
+}
